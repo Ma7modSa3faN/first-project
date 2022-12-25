ce4c6c487c09ed8c1ae8227ad373370e4f128434
Mahmoud Safaan <you@example.com>
2022-12-25 23:03:44 +0200

    added code file

---
+++ mahmoud.java
@@ -0,0 +1,8 @@
+public class mahmoud {
+    public static void main (String[] args) {
+        System.out.println("Mahmoud Safaan");
+        System.out.println(" My Project");
+
+        
+    }
+}
